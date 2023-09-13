d3eebf26b23814533228138088e882e7679aa448
Andrii <scoandrey@gmail.com>
2023-09-12 22:14:08 -0400

    saveaa

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { GlobalStyles } from 'Styles/GlobalStyles';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <GlobalStyles/>
     <App />
   </BrowserRouter>
 );
